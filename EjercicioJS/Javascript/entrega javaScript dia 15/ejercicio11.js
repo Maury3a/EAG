7183cd4f1aab40458d91115954c365411e6819a8
Maury3a <36621214+Maury3a@users.noreply.github.com>
2019-01-15 01:10:49 +0100

    Add files via upload

--- EjercicioJS/Javascript/entrega javaScript dia 15/ejercicio11.js
+++ EjercicioJS/Javascript/entrega javaScript dia 15/ejercicio11.js
@@ -1,14 +1,14 @@
 var filas=prompt("Escribe el numero de filas");
 var columnas=prompt("Escribe el numero de columnas");
 
-var contador=1;
+var numero=1;
 
 document.write("<table border='1px'>");
 for(i=1;i<=filas;i++){ //creamos las filas con el for
     document.write("<tr>");
     for(m=1;m<=columnas;m++){ //creamos las columnas
-        document.write("<td>"+contador+"</td>");
-        contador++; 
+        document.write("<td>"+numero+"</td>");
+        numero++; 
     }
     document.write("</tr>");
 }
