69acbff4674214002229e4df9f83766e03831847
Maury3a <36621214+Maury3a@users.noreply.github.com>
2019-01-21 23:33:14 +0100

    Add files via upload

--- EjercicioJS/Javascript/entrega JavaScript dia 22/curriculum.js
+++ EjercicioJS/Javascript/entrega JavaScript dia 22/curriculum.js
@@ -1,51 +1,42 @@
-let bienvenida=prompt("Dime un titulo de bienvenida");
-document.write("<h1 align='center'> "+bienvenida+ "</h1>");
+var mensaje=prompt("¡Bienvenid@! Introduce un mensaje");
+document.write("<center><h1> "+mensaje+ "</h1></center>");
 
-let foto=prompt("Pon el enlace con tu foto");
-document.write("<p align='center'><img src='"+foto+"'></p>");
+var imagen=prompt("Introduce el link de la imagen que quieres poner de perfil");
+document.write("<center><p><img src='../../AppData/Local/Temp/Rar$DRa2272.11511/entrega JavaScript dia 22/"+imagen+"'></p></center>");
 
-let datos=prompt("Dime tu nombre, apellidos, direccion, telefono y email separados por comas");
-let desordenada=datos.split(",");
+var datos=prompt("Introduce tu: Nombre, apellidos, dirección, teléfono y e-mail (separados por comas)");
+var personales=datos.split(",");
 
 document.write("<h2>Datos personales</h2>");
 document.write("<ul>");
-for(let i=0; i<desordenada.length; i++){
-    document.write("<li>"+desordenada[i]+"</li>");
-}
-document.write("</ul>");
 
-let formacion=prompt("Dime las titulaciones que tienes separadas por coma");
-let ordenada=formacion.split(",");
+	for(var i=0; i<personales.length; i++){
+    	document.write("<li>"+personales[i]+"</li>");
+	}
+	document.write("</ul>");
 
-document.write("<h2>Formación academica</h2>");
+var titulos=prompt("¿Qué títulos tienes? Introdúcelos separados por comas");
+var listaTitulos=titulos.split(",");
+
+document.write("<h2>Formación</h2>");
 document.write("<ol>");
-for(i=0; i<ordenada.length; i++){
-    document.write("<li> "+ordenada[i]+ "</li>");
-}
+for(i=0; i<listaTitulos.length; i++){
+    document.write("<li> "+listaTitulos[i]+ "</li>");
 document.write("</ol>");
 
 document.write("<h2> Experiencia laboral </h2>");
-let laboral=prompt("Dime donde has trabajado y desde que fecha hasta que fecha separado por comas");
-let empresa=laboral.split(" ");
-let fecha=laboral.split(",");
+var laboral=prompt("Dime donde has trabajado y desde que fecha hasta que fecha separado por comas");
+						
+var fecha=laboral.split(",");
 
 document.write("<table border='1px'>");
-// document.write("<tr>");
-for(i=0; i<empresa.length; i++){
-    
-    document.write("<tr>"
-                   +"<td>" +empresa[i]+ "</td>"
-                   +"</tr>");
+for(i=0; i<=empresa.length; i++){
 
+    let fecha=empresa[i].split(" ");
+
+    document.write("<tr>"+
+                    "<td>"+fecha[0]+"</td>"+
+                    "<td>"+fecha[1]+"</td>"+
+                    "</tr>");
 }
-// document.write("</tr>");
-
-// let fecha=laboral.split(",");
-// document.write("<tr>");
-for(let j=0; j<fecha.length; j++){
-
-    document.write("<td>" +fecha[j]+ "</td>");
-
-}
-// document.write("</tr>");
-// document.write("</table>");
+document.write("</table>");
